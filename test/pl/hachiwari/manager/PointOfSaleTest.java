fc862e333ef7e324558bbb94e57948b33948cae5
Tomasz Kurek <kurektom96@gmail.com>
2018-05-10 01:26:18 +0200

    update

--- test/pl/hachiwari/manager/PointOfSaleTest.java
+++ test/pl/hachiwari/manager/PointOfSaleTest.java
@@ -1,7 +1,22 @@
 package pl.hachiwari.manager;
 
+import pl.hachiwari.model.Product;
+
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
 
 public class PointOfSaleTest {
 
+
+
+    private static LinkedList<Product> createOrder(Object ... list) {
+        LinkedList<Product> result = new LinkedList<>();
+
+        for (Object o : list) {
+            result.add((Product) o);
+        }
+
+        return result;
+    }
 }
